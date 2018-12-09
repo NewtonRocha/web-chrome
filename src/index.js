0344eec482717c11f3bb21309be6e3de0a6065b0
Newton Rocha <newton.rocha@rappi.com>
2018-12-09 02:52:17 +0000

    greeting message test 6

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import style from "./index.css"
 import TemplateList from "./components/TemplateList";
 
 
+var parentOrigin;
+
+window.addEventListener('message', function greetingHandler(event) {
+  // This iframe only allows a gmail page to talk to it. Note that other pages
+  // on the internet could create an iframe with a url to this page and work for
+  // people with this extension installed, so this check is still important.
+  if (event.data === 'greeting' ) {
+    window.removeEventListener('message', greetingHandler, false);
+    parentOrigin = event.origin;
+    console.log(parentOrigin);
+  }
+}, false);
+
+
 const Index = () => {
   return <div>
     <TemplateList></TemplateList>
